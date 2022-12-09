12a2dcb9b8d2a0332960596063cbd77301b27868
bluengo <bluengopriet@gmail.com>
2022-12-09 17:50:55 +0100

    Day 8 Complete

--- day8/part2/main.py
+++ day8/part2/main.py
@@ -4,9 +4,11 @@ import numpy as np
 
 def main():
     trees = []
+    results = []
     
     #Open inputs
-    with open("../example.txt", "r") as f:
+    with open("../inputs-day8.txt", "r") as f:
+    #with open("../example.txt", "r") as f:
         for line in f.readlines():
             trees.append([int(i) for i in line[:-1]])
     f.close()
@@ -17,29 +19,67 @@ def main():
     num_rows = forest.shape[0]
     num_cols = forest.shape[1]
     
-    print(forest[:,1]) # [0 5 5 3 5]
-    print(forest[:3,1]) # [0 5 5]
-    print(forest[1,:]) # [2 5 5 1 2]
-    print(forest[:2,1]) # [0 5] # Column with index 1, from row 0 (:) and take 2
-    print(forest[:3,1]) # [0 5 5]
-    
     for row in range(0, num_rows):
-        for col in range(0, num_cols):
-            
-            print(f"\nNumber: {forest[row][col]}")
-            
+        for col in range(0, num_cols): 
+ 
             left = forest[row,:col]
-            print(f"left: {left}")
-
             right = forest[row,(col+1):]
-            print(f"right: {right}")
-
             top = forest[:row,col]
-            print(f"top: {top}")
-
             bottom = forest[(row+1):,col]
-            print(f"bottom: {bottom}")
 
+            # Calculate trees
+            result = checkVisibility(
+                forest[row][col],
+                left,
+                right,
+                top,
+                bottom)
+            
+            results.append(result)
+
+    # Print highest score:
+    print(f"The max is: {max(results)}")
+
+
+def checkVisibility(n, lft, rgt, top, btm):
+    
+    # Check left
+    lcount = 0
+    for l in np.flip(lft):
+        if l < n:
+            lcount += 1
+        else:
+            lcount += 1
+            break
+    
+    # Check right
+    rcount = 0
+    for r in rgt:
+        if r < n:
+            rcount += 1
+        else:
+            rcount += 1
+            break
+    
+    # Check top
+    tcount = 0
+    for t in np.flip(top):
+        if t < n:
+            tcount += 1
+        else:
+            tcount += 1
+            break
+    
+    # Check bottom
+    bcount = 0
+    for b in btm:
+        if b < n:
+            bcount += 1
+        else:
+            bcount += 1
+            break
+
+    return lcount * rcount * tcount * bcount
 
 if __name__ == "__main__":
     main()
