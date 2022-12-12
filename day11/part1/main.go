2e08807b029b1e244537d3a445ca2d06a4270ab4
bluengo <bluengopriet@gmail.com>
2022-12-12 14:52:34 +0100

    Add Day 11 draft

---
+++ day11/part1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type Monkey struct {
+	number         int
+	starting_items []int
+	operation      string
+	test           string
+	if_true        string
+	if_false       string
+}
+
+func main() {
+	fmt.Println("Hello Monkey")
+}
